c41185659a2fbf59dfb4ed7ee3f03731fcd74ea7
James Monger <jameskmonger@hotmail.co.uk>
2016-05-06 23:50:04 +0100

    Test adding mutliples of 360

--- test.js
+++ test.js
@@ -2,3 +2,15 @@
 
 const test = require('modunit');
 const safedegrees = require('./index');
+
+test('adding multiples of 360 returns original', (t, original, next) => {
+
+    let output = safedegrees(original, next);
+    t.assert.equal(original, output);
+
+}, [
+    [ 0, 360 ],
+    [ 10, 360 ],
+    [ 270, 720 ],
+    [ 359, 1080 ]
+]);
